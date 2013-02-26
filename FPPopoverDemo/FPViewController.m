18b5283a3340bd29e8feeeaddffdd807fb322187
Alvise Susmel <alvise@50pixels.com>
2013-02-26 12:06:01 +0000

    no title

--- FPPopoverDemo/FPViewController.m
+++ FPPopoverDemo/FPViewController.m
@@ -60,7 +60,7 @@
     DemoTableController *controller = [[DemoTableController alloc] initWithStyle:UITableViewStylePlain];
     controller.delegate = self;
     popover = [[FPPopoverController alloc] initWithViewController:controller];
-
+    
     popover.tint = FPPopoverDefaultTint;
     
     
